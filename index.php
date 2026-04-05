7c04b5b1e88e807c431094a733b94f2e2d8f44b7
Caitlin Waterworth <s2328610@bioinfmsc8.bio.ed.ac.uk>
2026-04-05 18:31:31 +0100

    Home page

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Protein Analysis Tool</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<div class="navbar">
+    <a href="index.php">Home</a>
+    <a href="aves.php">Example Dataset</a>
+    <a href="analysis_UI.php">Input Page</a>
+    <a href="credits.php">Credit Page</a>
+    <a href="about.php">About</a>
+    <a href="help.php">Help</a>
+</div>
+
+<div class="container">
+
+    <h1>Protein Sequence Analysis Website</h1>
+
+    <p>Welcome to the protein analysis tool.</p>
+
+</div>
+
+</body>
+</html>
